a668abe7152395039aed322ba9a7d311aba6ba46
Diera2710 <dibrienna.rauseuky@ui.ac.id>
2025-08-27 17:22:59 +0700

    finalization

--- football_news/settings.py
+++ football_news/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",'dibrienna-rauseuky-footballnews.pbp.cs.ui.ac.id' ]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
